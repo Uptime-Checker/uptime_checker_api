f2d2d03943a52027ed355ad2c60d90969a6470dc
Hasan <mr.k779@outlook.com>
2022-07-19 20:25:11 +0600

    Update header_auth.ex

--- lib/uptime_checker_web/plugs/header_auth.ex
+++ lib/uptime_checker_web/plugs/header_auth.ex
@@ -2,18 +2,24 @@ defmodule UptimeCheckerWeb.Plugs.HeaderAuth do
   import Plug.Conn
   require Logger
 
-  @api_key "x_api_key"
+  @api_key "X_API_KEY"
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     body = Jason.encode!(%{error: "unauthorized"})
+    header_key = List.first(get_req_header(conn, String.downcase(@api_key)))
 
-    if get_req_header(conn, @api_key) != System.get_env(@api_key) do
+    unless header_key == System.get_env(@api_key) do
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(:unauthorized, body)
       |> halt()
+    else
+      conn
     end
   end
+
+  # defp unauthorizedResp() do
+  # end
 end
