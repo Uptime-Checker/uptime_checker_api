eb1dd43fe50fdda1e7ac30a4ab5ec36452c8cc75
Hasan <mr.k779@outlook.com>
2022-07-22 01:34:31 +0600

    Updated monitor region

--- lib/uptime_checker/schema/monitor_region.ex
+++ lib/uptime_checker/schema/monitor_region.ex
@@ -5,6 +5,11 @@ defmodule UptimeChecker.Schema.MonitorRegion do
   alias UptimeChecker.Schema.WatchDog.Monitor
 
   schema "monitor_region_junction" do
+    field :last_checked_at, :utc_datetime
+    field :next_check_at, :utc_datetime
+    field :consequtive_failure, :integer
+    field :consequtive_recovery, :integer
+
     belongs_to(:monitor, Monitor)
     belongs_to(:region, Region)
 
--- priv/repo/migrations/20220720213033_create_monitor_region.exs
+++ priv/repo/migrations/20220720213033_create_monitor_region.exs
@@ -3,6 +3,11 @@ defmodule UptimeChecker.Repo.Migrations.CreateMonitorRegion do
 
   def change do
     create table(:monitor_region_junction) do
+      add :last_checked_at, :utc_datetime
+      add :next_check_at, :utc_datetime
+      add :consequtive_failure, :integer, default: 0
+      add :consequtive_recovery, :integer, default: 0
+
       add :monitor_id, references(:monitors, on_delete: :delete_all)
       add :region_id, references(:regions, on_delete: :delete_all)
 
@@ -11,6 +16,7 @@ defmodule UptimeChecker.Repo.Migrations.CreateMonitorRegion do
 
     create index(:monitor_region_junction, [:monitor_id])
     create index(:monitor_region_junction, [:region_id])
+    create index(:monitor_region_junction, [:last_checked_at, :next_check_at])
     create unique_index(:monitor_region_junction, [:monitor_id, :region_id])
   end
 end
