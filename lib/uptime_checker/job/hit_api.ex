4a0140e3f07cd0321eaf172b3252c0b500cbf32f
Hasan <mr.k779@outlook.com>
2022-07-26 04:59:17 +0600

    Update hit_api.ex

--- lib/uptime_checker/job/hit_api.ex
+++ lib/uptime_checker/job/hit_api.ex
@@ -1,7 +1,10 @@
 defmodule UptimeChecker.Job.HitApi do
-  def work(monitor_region_id) do
-    IO.inspect(monitor_region_id)
+  alias UptimeChecker.WatchDog
 
+  def work(monitor_region_id) do
+    monitor_region = WatchDog.get_monitor_region(monitor_region_id)
+
+    IO.inspect(monitor_region)
     :ok
   end
 end
