2c948bc846d51e49f01f09e2417f75c4337d73d0
Hasan <mr.k779@outlook.com>
2022-08-04 22:16:17 +0600

    New migration

---
+++ priv/repo/migrations/20220804153037_create_notification_channels.exs
@@ -0,0 +1,7 @@
+defmodule UptimeChecker.Repo.Migrations.CreateNotificationChannels do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
---
+++ priv/repo/migrations/20220804153041_create_notifications.exs
@@ -0,0 +1,7 @@
+defmodule UptimeChecker.Repo.Migrations.CreateNotifications do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
