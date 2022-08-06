7629d415626c853939a1e2090ee717ce18314018
Hasan <mr.k779@outlook.com>
2022-08-07 04:15:34 +0600

    Update current_user.ex

--- lib/uptime_checker/authentication/current_user.ex
+++ lib/uptime_checker/authentication/current_user.ex
@@ -6,10 +6,16 @@ defmodule UptimeChecker.Guardian.CurrentUser do
   def call(conn, _opts) do
     current_user = current_resource(conn)
 
-    Sentry.Context.set_user_context(%{
-      id: current_user.id,
-      email: current_user.email
-    })
+    case current_user do
+      %{id: _id} ->
+        Sentry.Context.set_user_context(%{
+          id: current_user.id,
+          email: current_user.email
+        })
+
+      nil ->
+        nil
+    end
 
     assign(conn, :current_user, current_user)
   end
