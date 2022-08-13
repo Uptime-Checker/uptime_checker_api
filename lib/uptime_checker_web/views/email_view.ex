4cc5565d0ccebe74e6bd8fdcc1abb86e5dd760b4
Hasan <mr.k779@outlook.com>
2022-08-13 22:26:36 +0600

    Basic email for monitor outage

--- lib/uptime_checker_web/views/email_view.ex
+++ lib/uptime_checker_web/views/email_view.ex
@@ -7,6 +7,13 @@ defmodule UptimeCheckerWeb.EmailView do
     down(monitor.down)
   end
 
+  def format_time(time) do
+    time |> Timex.format!("{RFC1123}")
+  end
+
+  def difference_between_two_times() do
+  end
+
   defp down(is_down) when is_down == true, do: Constant.Text.down()
   defp down(is_down) when is_down == false, do: Constant.Text.up()
 end
