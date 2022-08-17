fd5fcf9f52b9039fc40ca87aee78a1223de2bd90
Hasan <mr.k779@outlook.com>
2022-08-18 05:05:26 +0600

    Update run_checks_on_starup.ex

--- lib/uptime_checker/job/run_checks_on_starup.ex
+++ lib/uptime_checker/job/run_checks_on_starup.ex
@@ -1,5 +1,12 @@
 defmodule UptimeChecker.Job.RunChecksOnStarup do
+  alias UptimeChecker.WatchDog
+
   def work() do
+    handle_active_monitors()
     :ok
+  end
+
+  defp handle_active_monitors() do
+    %{entries: entries, metadata: metadata} = WatchDog.list_monitor_region_for_active_monitors()
   end
 end
