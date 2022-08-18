10d8a4e8d6850a686c50d1ba2305f80ea30027fc
Hasan <mr.k779@outlook.com>
2022-08-19 03:24:31 +0600

    Update run_checks_on_starup.ex

--- lib/uptime_checker/job/run_checks_on_starup.ex
+++ lib/uptime_checker/job/run_checks_on_starup.ex
@@ -2,6 +2,7 @@ defmodule UptimeChecker.Job.RunChecksOnStarup do
   use Timex
   alias UptimeChecker.WatchDog
   alias UptimeChecker.Helper.String
+  alias UptimeChecker.TaskSupervisor
 
   def work() do
     handle_active_monitors(nil)
@@ -13,15 +14,25 @@ defmodule UptimeChecker.Job.RunChecksOnStarup do
 
     now = NaiveDateTime.utc_now()
 
-    Enum.each(entries, fn monitor_region ->
-      later = Timex.shift(now, seconds: monitor_region.monitor.interval)
-      WatchDog.update_monitor_region(monitor_region, %{next_check_at: later})
-    end)
+    stream =
+      Task.Supervisor.async_stream(
+        TaskSupervisor,
+        entries,
+        fn entry -> update_monitor_region(entry, now) end,
+        max_concurrency: 5
+      )
+
+    Enum.to_list(stream)
 
     with metadata_after_cursor <- metadata.after do
       unless String.blank?(metadata_after_cursor) do
         handle_active_monitors(metadata_after_cursor)
       end
     end
+  end
+
+  defp update_monitor_region(monitor_region, now) do
+    later = Timex.shift(now, seconds: monitor_region.monitor.interval)
+    WatchDog.update_monitor_region(monitor_region, %{next_check_at: later})
   end
 end
