41cc9511fe7dada03154921a2d2d7877d9a8372b
Hasan <mr.k779@outlook.com>
2022-08-21 05:02:42 +0600

    WIP Join user

--- lib/uptime_checker_web/controllers/api/v1/invitation_controller.ex
+++ lib/uptime_checker_web/controllers/api/v1/invitation_controller.ex
@@ -19,6 +19,24 @@ defmodule UptimeCheckerWeb.Api.V1.InvitationController do
   end
 
   def join(conn, params) do
+    with {:ok, invitation} <- InvitationService.verify_invitation(params["email"], params["code"]) do
+      with {:ok, user} <- Auth.get_by_email_with_org(invitation.email) do
+        access_token = after_email_link_login_successful(guest_user, updated_user)
+
+        conn
+        |> put_status(:accepted)
+        |> json(%{access_token: access_token})
+      else
+        {:error, :not_found} ->
+          with {:ok, %User{} = user} <- Customer.create_user_for_provider(params) do
+            access_token = after_email_link_login_successful(guest_user, user)
+
+            conn
+            |> put_status(:created)
+            |> json(%{access_token: access_token})
+          end
+      end
+    end
   end
 
   def get(conn, params) do
@@ -26,5 +44,15 @@ defmodule UptimeCheckerWeb.Api.V1.InvitationController do
       conn
       |> render("show.json", %{invitation: invitation, user: user})
     end
+  end
+
+  defp after_email_link_login_successful(guest_user, user) do
+    Task.Supervisor.start_child(TaskSupervisor, Auth, :delete_guest_user, [guest_user], restart: :transient)
+    {:ok, access_token, _claims} = encode_and_sign(user)
+    access_token
+  end
+
+  defp encode_and_sign(user) do
+    Guardian.encode_and_sign(user, %{}, token_type: "access", ttl: {180, :day})
   end
 end
