f4f0a56411c123b203d39febf18a086ebfc480d6
Hasan <mr.k779@outlook.com>
2022-09-03 01:30:39 +0600

    Update webhook.ex

--- lib/uptime_checker/module/stripe/webhook.ex
+++ lib/uptime_checker/module/stripe/webhook.ex
@@ -1,4 +1,7 @@
 defmodule UptimeChecker.Module.Stripe.Webhook do
+  use Timex
+  require Logger
+
   @behaviour Stripe.WebhookHandler
 
   alias UptimeChecker.{Customer, ProductService, Payment}
@@ -29,8 +32,31 @@ defmodule UptimeChecker.Module.Stripe.Webhook do
 
   @impl true
   def handle_event(%Stripe.Event{type: @invoice_created} = event) do
+    data = event.data
+    line = Enum.at(data.lines.data, 0)
     {:ok, user} = Customer.get_customer_by_payment_id(event.data.customer)
-    {:ok, plan} = ProductService.get_plan_with_product_from_external_id(event.data.customer)
+    {:ok, plan} = ProductService.get_plan_with_product_from_external_id(line.price.id)
+
+    params = %{
+      price: round(data.amount_due / 100),
+      currency: data.currency,
+      external_id: data.id,
+      external_customer_id: data.customer,
+      url: data.lines.url,
+      status: data.status,
+      paid: data.paid,
+      from: Timex.from_unix(data.period_start),
+      to: Timex.from_unix(data.period_end),
+      is_trial: false,
+      plan: plan,
+      product: plan.product,
+      organization: user.organization
+    }
+
+    {:ok, receipt} = Payment.create_receipt(params)
+    Logger.info("Receipt created #{receipt.id} for org #{user.organization.id}, plan #{plan.id}")
+
+    :ok
   end
 
   @impl true
