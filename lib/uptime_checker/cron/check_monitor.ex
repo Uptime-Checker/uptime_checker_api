0749bd64c89a198b8e0285ea74914b6c04ddc785
Hasan <mr.k779@outlook.com>
2022-09-29 03:52:42 +0600

    Update check_monitor.ex

--- lib/uptime_checker/cron/check_monitor.ex
+++ lib/uptime_checker/cron/check_monitor.ex
@@ -1,13 +1,20 @@
 defmodule UptimeChecker.Cron.CheckMonitor do
+  require Logger
+
   alias UptimeChecker.Worker
   alias UptimeChecker.WatchDog
+  alias UptimeChecker.Helper.Strings
 
   def work do
+    tracing_id = Strings.random_string(10)
     # Load all the monitors for each region that were supposed to be executed
     # in the last 5 seconds upto next 10 seconds. This cron runs every 10 seconds
     monitor_regions = WatchDog.list_monitor_region(-5, +10)
 
+    Logger.info("#{tracing_id} running check monitor")
+
     Enum.map(monitor_regions, fn monitor_region ->
+      Logger.info("#{tracing_id} enqueue -> #{monitor_region.id}")
       monitor_region |> Worker.HitApiAsync.enqueue()
     end)
   end
