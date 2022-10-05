352dbf1bc3889a238c9408caf8141875ca956b1d
Hasan <mr.k779@outlook.com>
2022-10-05 22:58:00 +0600

    Start monitor async

--- lib/uptime_checker/job/start_monitor.ex
+++ lib/uptime_checker/job/start_monitor.ex
@@ -1,27 +1,48 @@
 defmodule UptimeChecker.Job.StartMonitor do
   require Logger
 
-  alias UptimeChecker.WatchDog
   alias UptimeChecker.Job.HitApi
   alias UptimeChecker.Helper.Strings
-  alias UptimeChecker.Service.MonitorService
+  alias UptimeChecker.{WatchDog, Customer}
+  alias UptimeChecker.Schema.WatchDog.Check
+  alias UptimeChecker.Service.{MonitorService, RegionService}
+  alias UptimeChecker.Event.{HandleErrorLog, HandleApiResponse}
 
   def work(monitor_id) do
     tracing_id = Strings.random_string(10)
     monitor = MonitorService.get(monitor_id)
 
-    with {u_secs, result} <- HitApi.hit(tracing_id, monitor) do
-      duration = round(u_secs / 1000)
+    with {:ok, org} <- Customer.get_user_contact_by_id(monitor.organization_id),
+         {:ok, region} <- RegionService.get_current_region(),
+         {:ok, check} <- WatchDog.create_check(%{}, monitor, region, org) do
+      with {u_secs, result} <- HitApi.hit(tracing_id, monitor) do
+        duration = round(u_secs / 1000)
 
-      case result do
-        {:ok, %HTTPoison.Response{} = _response} ->
-          WatchDog.create_monitor_regions(monitor)
+        case result do
+          {:ok, %HTTPoison.Response{} = response} ->
+            Logger.info(
+              "#{tracing_id} RESPONSE #{check.monitor.url} CODE ==> #{response.status_code} DURATION ==> #{duration}"
+            )
 
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          Logger.error("#{tracing_id} API Request failed #{monitor.url}, reason #{reason}")
+            handle_response(tracing_id, response, check)
+
+          {:error, %HTTPoison.Error{reason: reason}} ->
+            Logger.error("#{tracing_id} API Request failed #{monitor.url}, reason #{reason}")
+            HandleErrorLog.finalize(tracing_id, reason, check)
+        end
       end
     end
 
     :ok
+  end
+
+  def handle_response(tracing_id, %HTTPoison.Response{} = response, %Check{} = check) do
+    case HandleApiResponse.handle(response, check.monitor) do
+      {:ok, _success} ->
+        WatchDog.create_monitor_regions(check.monitor)
+
+      {:error, code} ->
+        HandleErrorLog.create(tracing_id, response.body, response.status_code, check, code)
+    end
   end
 end
--- lib/uptime_checker/service/region_service.ex
+++ lib/uptime_checker/service/region_service.ex
@@ -2,6 +2,7 @@ defmodule UptimeChecker.Service.RegionService do
   import Ecto.Query, warn: false
 
   alias UptimeChecker.Repo
+  alias UptimeChecker.Constant.Env
   alias UptimeChecker.Schema.Region
   alias UptimeChecker.Error.RepoError
 
@@ -10,7 +11,18 @@ defmodule UptimeChecker.Service.RegionService do
     |> Repo.get_by(default: true)
     |> case do
       nil -> {:error, RepoError.region_not_found() |> ErrorMessage.not_found(%{default: true})}
-      organization -> {:ok, organization}
+      region -> {:ok, region}
+    end
+  end
+
+  def get_current_region() do
+    region_key = Env.current_region() |> System.get_env()
+
+    Region
+    |> Repo.get_by(key: region_key)
+    |> case do
+      nil -> {:error, RepoError.region_not_found() |> ErrorMessage.not_found(%{key: region_key})}
+      region -> {:ok, region}
     end
   end
 
