7ba1f3184c8b6d6835b8ce5954beb60eab54f5ff
Hasan <mr.k779@outlook.com>
2022-12-30 21:02:03 +0600

    Update sync_products.ex

--- lib/uptime_checker/cron/sync_products.ex
+++ lib/uptime_checker/cron/sync_products.ex
@@ -5,41 +5,41 @@ defmodule UptimeChecker.Cron.SyncProducts do
   def work do
     Logger.info("running sync products cron")
 
-    {:ok, products} = Stripe.Product.list()
-    {:ok, prices} = Stripe.Price.list()
+    with {:ok, products} <- Stripe.Product.list(),
+         {:ok, prices} <- Stripe.Price.list() do
+      Enum.each(products.data, fn product ->
+        with {:ok, product} <-
+               ProductService.create_product(%{
+                 name: product.name,
+                 description: product.description,
+                 external_id: product.id,
+                 tier: product.metadata["tier"]
+               }) do
+          filtered_prices =
+            Enum.filter(prices.data, fn price ->
+              if price.product == product.external_id do
+                price
+              end
+            end)
 
-    Enum.each(products.data, fn product ->
-      with {:ok, product} <-
-             ProductService.create_product(%{
-               name: product.name,
-               description: product.description,
-               external_id: product.id,
-               tier: product.metadata["tier"]
-             }) do
-        filtered_prices =
-          Enum.filter(prices.data, fn price ->
-            if price.product == product.external_id do
-              price
-            end
+          Enum.each(filtered_prices, fn price ->
+            plan_type =
+              if price.recurring.interval == "year" do
+                :yearly
+              else
+                :monthly
+              end
+
+            ProductService.create_plan(%{
+              price: price.unit_amount / 100,
+              external_id: price.id,
+              type: plan_type,
+              product: product
+            })
           end)
-
-        Enum.each(filtered_prices, fn price ->
-          plan_type =
-            if price.recurring.interval == "year" do
-              :yearly
-            else
-              :monthly
-            end
-
-          ProductService.create_plan(%{
-            price: price.unit_amount / 100,
-            external_id: price.id,
-            type: plan_type,
-            product: product
-          })
-        end)
-      end
-    end)
+        end
+      end)
+    end
 
     :ok
   end
